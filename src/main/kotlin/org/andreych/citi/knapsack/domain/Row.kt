928312b23f6c458405b13bf7c1dc86e789158e04
Oleg Andreych <kjiec4@gmail.com>
2017-10-26 22:12:43 +0500

    Now cost calculated as value * 4.3 in according to citi celect program rules.

--- src/main/kotlin/org/andreych/citi/knapsack/domain/Row.kt
+++ src/main/kotlin/org/andreych/citi/knapsack/domain/Row.kt
@@ -15,20 +15,26 @@ data class Row(val date: LocalDate, val name: String, val cost: Int, val value: 
 
         private val PATTERN: Regex = Regex("^(?<date>\\d{2}/\\d{2}/\\d{4})(?<name>.*?)(?<cost>[\\d,]*?\\.\\d{2})\\s*?(?<value>[\\d,]*?\\.\\d{2}) \\w{3}\$")
         private val DATE_PATTERN = "dd/MM/yyyy"
+        private val COST_MULTIPLICATOR = BigDecimal("4.3")
 
         fun parse(line: String): Row {
 
             val groups = Row.PATTERN.find(line)?.groups ?: throw IllegalArgumentException("Cannot parse line $line.")
             val date = LocalDate.parse(groups[1]?.value, DateTimeFormatter.ofPattern(DATE_PATTERN))
             val name = groups[2]!!.value
-            val cost = matchToInt(groups, 3)
-            val value = matchToInt(groups, 4)
+            val value = normalizeValue(groups)
+            val cost = calculateCost(groups)
 
             return Row(date, name, cost, value)
         }
 
-        private fun matchToInt(groups: MatchGroupCollection, s: Int): Int {
-            val value = groups[s]!!.value.replace(",", "")
+        private fun calculateCost(groups: MatchGroupCollection): Int {
+            val value = groups[4]!!.value.replace(",", "")
+            return BigDecimal(value).multiply(COST_MULTIPLICATOR).movePointRight(2).toInt()
+        }
+
+        private fun normalizeValue(groups: MatchGroupCollection): Int {
+            val value = groups[4]!!.value.replace(",", "")
             return BigDecimal(value).movePointRight(2).toInt()
         }
     }
